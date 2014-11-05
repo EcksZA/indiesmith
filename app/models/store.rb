280529ffd2914cb70d0e69f199c3a57ec4bb0295
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 14:31:22 -0800

    validates presence of email

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
 	validates :name, :country, presence: true
+	validates :email, presence: true
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Store do
 	it { should validate_presence_of :name }
 	it { should validate_presence_of :country }
+	it { should validate_presence_of :email }
 end
