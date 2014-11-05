7cadbdddb0baf2662e45e7cdb58db30becf253a3
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 15:51:00 -0800

    validates presence of store_id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	validates :name, :description, presence: true
-	validates :price, presence: true
+	validates :price, :store_id, presence: true
 
 	belongs_to :store
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ describe Product do
 	it { should validate_presence_of :name }
 	it { should validate_presence_of :description }
 	it { should validate_presence_of :price }
+	it { should validate_presence_of :store_id }
 
 	it { should belong_to :store }
 end
