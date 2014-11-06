1e7cd1e04e8ef89766f6259d495106b2b0830664
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 18:26:38 -0800

    allow for photos to be added via paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ActiveRecord::Base
 	validates :price, :store_id, presence: true
 
 	belongs_to :store
+
+	has_attached_file :photo, styles: { medium: '300x300>' },
+					url: '/assets/products/:id/:style/:basename.:extension',
+					path: ':rails_root/public/assets/products/:id/:style/:basename.:extension'
+
+	validates_attachment_presence :photo
+	validates_attachment_content_type :photo, content_type: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif']
+
 end
