a86d6ef1a026cf77a1c2c1c2245bdccdea85cdce
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 22:18:27 -0800

    add password to stores table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106060412) do
+ActiveRecord::Schema.define(version: 20141106061654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141106060412) do
     t.string "phone"
     t.string "contact_email"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
