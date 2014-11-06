a90b6ad41b75674df94d0673f9f0530e5d798a0b
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 23:14:23 -0800

    add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   get 'signup', to: 'stores#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :products, only: [:index]
   resources :sessions, only: [:new, :create, :destroy]
