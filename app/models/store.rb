9c5d4dfc03993dd39c1ee0ea13f665274af4c177
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 22:09:08 -0800

    validates presence of contact_email in place of email

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
 	validates :name, :country, presence: true
-	validates :email, :phone, presence: true
+	validates :contact_email, :phone, presence: true
 
 	has_many :products
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Store do
 	it { should validate_presence_of :name }
 	it { should validate_presence_of :country }
-	it { should validate_presence_of :email }
+	it { should validate_presence_of :contact_email }
 	it { should validate_presence_of :phone }
 
 	it { should have_many :products }
