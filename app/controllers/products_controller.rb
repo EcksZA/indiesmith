7ef0c8355978f71305e65a2b004f7457bb818e8e
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 20:57:03 -0800

    add photo attribute to safe params in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
 
 private
 	def product_params
-		params.require(:product).permit(:name, :description, :price, :store_id)
+		params.require(:product).permit(:name, :description, :price, :photo, :store_id)
 	end
 end
