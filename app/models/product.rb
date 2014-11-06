c85bf2c76c62b9750bd8f06da90491617223638c
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-05 20:55:58 -0800

    add photo upload functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
 					url: '/assets/products/:id/:style/:basename.:extension',
 					path: ':rails_root/public/assets/products/:id/:style/:basename.:extension'
 
-	validates_attachment_presence :photo
+	# validates_attachment_presence :photo
 	validates_attachment_content_type :photo, content_type: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif']
 
 end
